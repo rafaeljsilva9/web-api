d250dba6d9fcf9c951c17ab70ec3657e457bf876
Rafael Silva <rafael.j.dasilva9@gmail.com>
2024-05-29 21:45:01 -0300

    feat: Introducing Express.js

--- src/server/application/api/routes/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import ordersRoutes from './orders.route';
-
-const routes = Router();
-routes.use('/orders', ordersRoutes);
-export default routes;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,23 +1,21 @@
-import express, { NextFunction, Request, Response } from "express";
 import { json, urlencoded } from "body-parser";
-import { CorsMiddleware } from "./application/api/middlewares/cors-middleware";
-import routes from './application/api/routes/routes';
+import express from "express";
 
-const APP_PORT = 3000;
 const app = express();
+const port = 3000;
 
-app.listen(APP_PORT, async () => {
-  console.log(`Express App Listening on Port ${APP_PORT}`);
-});
+// Middleware para parsear JSON
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.use((req: Request, res: Response, next: NextFunction) => new CorsMiddleware().use(req, res, next));
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.use(routes);
 
+// Rota de exemplo
+app.get('/', (req, res) => {
+  res.send('Hello Word!')
+});
 
-app.all("*", (req, res) => {
-  res.status(404).json({ error: "Resource not found" });
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
 
 export default app;
