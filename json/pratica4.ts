7d6695b228beaa56acaccac496f774671ec45fd3
João Lucas Oliveira Canhoto (CESAR) <jloc@cesar.org.br>
2024-06-03 15:36:10 -0300

    add json practice 4

---
+++ json/pratica4.ts
@@ -0,0 +1,5 @@
+import fs from 'fs';
+
+const inputJsonFilepath = `${__dirname}/entrada.json`;
+const outputJsonFilepath = `${__dirname}/saida.json`;
+const outputJsonFilepath2 = `${__dirname}/saida-legivel.json`;
