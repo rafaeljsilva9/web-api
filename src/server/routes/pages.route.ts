a164a1a01f2cea121a758ff6520b4888be9eadc5
Rafael Silva <rafael.j.dasilva9@gmail.com>
2024-06-06 14:04:27 -0300

    fix: Fix pages directory

--- src/server/routes/pages.route.ts
+++ src/server/routes/pages.route.ts
@@ -4,12 +4,12 @@ import path from "path";
 const routes = express.Router();
 
 routes.get("/index", (req, res) => {
-  const filePath = path.join(__dirname, "../../client", "index.html");
+  const filePath = path.join(__dirname, "../../client/pages", "index.html");
   res.status(200).sendFile(filePath);
 });
 
 routes.get("/movies", (req, res) => {
-  const filePath = path.join(__dirname, "../../client", "movies.html");
+  const filePath = path.join(__dirname, "../../client/pages", "movies.html");
   res.status(200).sendFile(filePath);
 });
 
